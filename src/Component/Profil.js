c4faf5d1b75a2b8d550e6ec985a1d4633d5f5ce2
chneina fatma <chneina.chneina@gmail.com>
2023-05-24 13:21:46 +0200

    second commit

--- src/Component/Profil.js
+++ src/Component/Profil.js
@@ -7,7 +7,7 @@ function Profil({ name, Age, prof, handleName }) {
             <h1>{name}</h1>
             <h1> {Age}</h1>
             <h4> {prof} </h4>
-          
+            <div className='but'><button onClick={() => handleName(prof)}>click to show prof </button></div>
             <Pic  >
                 <img  style={ { marginLeft: "27%", width:"50%"}}  
  src='profili.jpg' />
